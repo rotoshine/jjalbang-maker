3fbda53f73e9a546e97cbc206949e17412fc439c
Roto <roto@smartstudy.co.kr>
2017-07-10 15:16:06 +0900

    폰트 경로에 http 박혀있던 부분 제거

--- src/assets/fonts.js
+++ src/assets/fonts.js
@@ -6,31 +6,31 @@ export default [
   {
     name: '한나체',
     cssValue: 'Hanna',
-    importUrl: 'http://fonts.googleapis.com/earlyaccess/hanna.css'
+    importUrl: '//fonts.googleapis.com/earlyaccess/hanna.css'
   },
   {
     name: '나눔고딕',
     cssValue: 'Nanum Gothic',
-    importUrl: 'http://fonts.googleapis.com/earlyaccess/nanumgothic.css'
+    importUrl: '//fonts.googleapis.com/earlyaccess/nanumgothic.css'
   },
   {
     name: '나눔손글씨 붓',
     cssValue: 'Nanum Brush Script',
-    importUrl: 'http://fonts.googleapis.com/earlyaccess/nanumbrushscript.css'
+    importUrl: '//fonts.googleapis.com/earlyaccess/nanumbrushscript.css'
   },
   {
     name: '나눔 브러시 스크립트',
     cssValue: 'Nanum Brush Script',
-    importUrl: 'http://fonts.googleapis.com/earlyaccess/nanumbrushscript.css'
+    importUrl: '//fonts.googleapis.com/earlyaccess/nanumbrushscript.css'
   },
   {
     name: '제주 한라산',
     cssValue: 'Jeju Hallasan',
-    importUrl: 'http://fonts.googleapis.com/earlyaccess/jejuhallasan.css'
+    importUrl: '//fonts.googleapis.com/earlyaccess/jejuhallasan.css'
   },
   {
     name: '코펍 바탕체',
     cssValue: 'KoPub Batang',
-    importUrl: 'http://fonts.googleapis.com/earlyaccess/kopubbatang.css'
+    importUrl: '//fonts.googleapis.com/earlyaccess/kopubbatang.css'
   }
 ];
