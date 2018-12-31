0affaa90e2011093b31b8d2bd68ec557bac607b0
rotoshine <rotoshine@gmail.com>
2019-01-01 04:48:36 +0900

    update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'comma-dangle': 0,
-    'space-in-parens': 0
+    'space-in-parens': 0,
+    'semi': 0
   }
 }
